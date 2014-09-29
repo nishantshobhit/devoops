481d245394d4133e03f3f3fbef12369e87d0c6fc
root <root@ip-172-31-8-10.ap-southeast-1.compute.internal>
2014-09-29 18:47:28 +0000

    more updates to the tui website

--- js/tui.js
+++ js/tui.js
@@ -1,16 +1,38 @@
-function getJsonFromUrl() {
-  var query = location.search.substr(1);
+// gets query parameters
+function qp() {
   var result = {};
+  var url = document.URL;
+  query = url.split('?')[2];
   query.split("&").forEach(function(part) {
     var item = part.split("=");
     result[item[0]] = decodeURIComponent(item[1]);
   });
   return result;
 }
-function render(locations) { 
+
+// reverse geocode
+function reverseGeocode(lat, lng, callback) {
+  var latlng = new google.maps.LatLng(lat, lng);
+  var geocoder = new google.maps.Geocoder()
+  geocoder.geocode({'latLng': latlng}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+          callback(results);
+        } else {
+          alert('No results found');
+        }
+    } else {
+      console.log('geocoder api failed');
+    }
+  });
+}
+
+
+// renders a map iframe
+function createGMapsView(locations, center, zoom) { 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: new google.maps.LatLng(12.93, 77.59),
+    zoom: zoom,
+    center: new google.maps.LatLng(center['lat'], center['long']),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
@@ -32,7 +54,9 @@ function render(locations) {
     })(marker, i));
   }
 }
-function renderItems(type, state) {
+
+// renders reports and spotfixes pages
+function startRSView(type, action, state) {
   $(document).ready(function(){
     $.ajax({
       'url' : 'http://ec2-54-169-91-68.ap-southeast-1.compute.amazonaws.com/tui/'  + type + '/'
@@ -41,15 +65,17 @@ function renderItems(type, state) {
       for (var i=0; i<data.length; i++) {
         if (state == 'ALL' || data[i]['state'] == state) {
           var latlong = data[i]['location'].split(',');
-          var url = 'http://ec2-54-169-91-68.ap-southeast-1.compute.amazonaws.com/?#ajax/edit_' + type + '.html?id=' + data[i]['id'];
+          var url = 'http://ec2-54-169-91-68.ap-southeast-1.compute.amazonaws.com/?#ajax/' + action + '_' + type + '.html?id=' + data[i]['id'];
           locations.push({'label' : '<a target="_blank" href="' + url  + '">View</a>', 'lat' : latlong[0], 'long' : latlong[1]});
         }
       }
-      render(locations);
+      createGMapsView(locations, {'lat' : 12.93, 'long' : 77.59}, 12);
     });
   });
 }
-function renderFeed() {
+
+// renders feed items
+function startFeedView() {
   $(document).ready(function(){
     $.ajax({
       'url' : 'http://ec2-54-169-91-68.ap-southeast-1.compute.amazonaws.com/tui/feed/?q=all'
@@ -79,5 +105,49 @@ function renderFeed() {
     });
   });
 }
-function editReport(id) {
+
+function startRSEditView(type, approve, reject) {
+  $(document).ready(function(){
+    var id = qp()['id'];
+    $.ajax({ 'url' : 'http://ec2-54-169-91-68.ap-southeast-1.compute.amazonaws.com/tui/' + type + '/' + id})
+      .done(function(data){
+        var ts = new Date(0);
+        ts.setUTCSeconds(data['createdOn']/1000);
+        $('#report-description').html('What?: ' + data['desc']);
+        $('#report-current-state').html('Currently: ' + data['state']);
+        $('#report-reported-on').html('Reported on: ' + ts.toDateString());
+        var latlong = data['location'].split(',');
+        reverseGeocode(parseFloat(latlong[0]), parseFloat(latlong[1]), function(address) {
+          for (var i in address) {
+            var addr = address[i];
+            for (var j in addr['types']) {
+              if ('sublocality_level_1' == addr['types'][j]) {
+                $('#report-location').html(addr.formatted_address);
+              }
+            }
+          } 
+        });        
+        createGMapsView([{'label' : 'Foo', 'lat' : latlong[0], 'long' : latlong[1]}], {'lat' : latlong[0], 'long' : latlong[1]}, 17);
+        $('#approve').click(function(){
+          changeRSStatus(type, id, approve); // CONFIRM
+        });
+        $('#reject').click(function(){
+          changeRSStatus(type, id, reject);
+        });
+      });
+  });
+}
+
+function changeRSStatus(type, id, status) {
+  var data = {'state' : status, 'scheduled_on' : '2014-02-02 08:00:00'};
+  $.ajax({
+    'beforeSend' : function(xhrObj){
+      xhrObj.setRequestHeader("Content-Type","application/json");
+    },
+    'type' : 'POST',
+    'url' : 'http://ec2-54-169-91-68.ap-southeast-1.compute.amazonaws.com/tui/' + type + '/' + id,
+    'data' : JSON.stringify(data)
+  }).done(function(){
+    window.location.href = 'http://ec2-54-169-91-68.ap-southeast-1.compute.amazonaws.com/';
+  });
 }
